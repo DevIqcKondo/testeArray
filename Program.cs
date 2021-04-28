fd83dffb3bbe0c87b711f25a3f19d977e4a8ebb1
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-04-28 08:28:36 -0300

    Teste executado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace testeArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num=new int[]{1,5,10,55,65,80,99,106};
+            for(int i=0;i<num.Length;i++){
+                Console.WriteLine(num[i]);
+            }
+        }
+    }
+}
